6b1f68702ab00d8066f2f4c2defeb451f81e6e61
Abdallah Abufayad <abdallah.abufayad@student.ap.be>
2026-04-01 14:45:49 +0200

    interfaces zijn toegevoegd

---
+++ types.ts
@@ -0,0 +1,23 @@
+import { ObjectId } from "mongodb";
+
+export interface Users {
+    _id? : ObjectId,
+    email: string,
+    username: string,
+    password: string,
+    level: number,
+    about_me:string ,
+    profile_picture: string,
+    collection_more: Collection_more
+}
+
+export interface Collection_more {
+    collectionName:string,
+    allGames:AllGames[]
+}
+
+export interface AllGames {
+    gameId:string,
+    gameName:string,
+    gameImge:string
+}
