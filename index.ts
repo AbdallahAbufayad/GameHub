a807f09e5cf82025636e55659bd111e7b717bb9d
ElkeK <s167738@ap.be>
2026-04-22 08:11:11 +0000

    Added routers for profile.ejs, public-profile.ejs and login and register. Also made the database file and automatically connects with each startup of the server, also ends once exited.

--- index.ts
+++ index.ts
@@ -3,8 +3,11 @@ import dotenv from "dotenv";
 import path from "path";
 import { GamesApi, Results, Users, Game } from "./types";
 import { Collection, MongoClient, ObjectId } from "mongodb";
-import { profileRouter } from "./routers/profile.router";
+import { profileRoute, PublicProfileRoute } from "./routers/profile-router";
 import { ThemeMiddleware } from "./middleware/theme-middleware";
+import { registerRoute } from "./routers/register-router";
+import { loginRoute } from "./routers/login-router";
+import { connect } from "./database";
 
 dotenv.config();
 
@@ -18,9 +21,6 @@ app.set("views", path.join(__dirname, "views"));
 
 app.set("port", process.env.PORT || 3000);
 
-//Mongo client
-let client: MongoClient;
-let userCollection: Collection<Users>;
 
 const url: string = `https://api.rawg.io/api/games?key=30778c23f4f34908a65b042d94443ba7&dates=1969-01-01,2026-04-11`;
 
@@ -252,179 +252,12 @@ app.get("/game-info/:id", async (req, res) => {
   });
 });
 
-app.get("/register", (req, res) => {
-  const themaName: string = res.locals.themaName;
 
-  res.render("register", {
-    title: "Registreren",
-    themaName: themaName,
-    notification: notification,
-  });
-});
-app.get("/login", (req, res) => {
-  const themaName: string = res.locals.themaName;
 
-  res.render("login", {
-    title: "Inloggen",
-    themaName: themaName,
-    notification: notification,
-    loggedIn: loggedIn,
-  });
-});
-app.post("/login", async (req, res) => {
-  const themaName: string = res.locals.themaName;
-
-  const emailOrUsername: string = req.body.email_username.toLowerCase();
-  const password: string = btoa(req.body.password); //will be properly hashed after security class
-  const cityName: string = req.body.cityName;
-  const countryName: string = req.body.countryName;
-
-  try {
-    await client.connect();
-    const allUsers: Users[] = await userCollection.find().toArray();
-
-    const currentUser: Users | undefined = allUsers.find(
-      (u) =>
-        u.username.toLowerCase() === emailOrUsername ||
-        u.email.toLowerCase() === emailOrUsername,
-    );
-
-    let userId: ObjectId | undefined = currentUser?._id;
-
-    if (currentUser) {
-      if (
-        (currentUser.username.toLowerCase() === emailOrUsername ||
-          currentUser.email.toLowerCase()) &&
-        currentUser.password === password
-      ) {
-        notification =
-          "Je bent succesvol ingelogd! Je wordt direct doorgebracht naar de index pagina.";
-        loggedIn = true;
-        //localStorage.setItem("loggedIn", "true"); done with cookies
-        //localStorage.setItem("userId", userId?); done with cookies later
-        res.redirect("/login");
-        return;
-      } else {
-        notification = "Het ingevoerde wachtwoord is verkeerd.";
-        res.render("login", {
-          title: "Inloggen",
-          themaName: themaName,
-          notification: notification,
-          loggedIn: loggedIn,
-        });
-        return;
-      }
-    } else {
-      notification = "Gebruiker niet gevonden. Maak eerst een account aan!";
-      res.render("login", {
-        title: "Inloggen",
-        themaName: themaName,
-        notification: notification,
-        loggedIn: loggedIn,
-      });
-      return;
-    }
-  } catch (e) {
-    console.error("Something went wrong with the database.");
-  } finally {
-    await client.close();
-  }
-});
-
-app.post("/register", async (req, res) => {
-  const themaName: string = res.locals.themaName;
-
-  const email: string = req.body.email.toLowerCase();
-  const username: string = req.body.username.toLowerCase();
-  const password: string = btoa(req.body.password); //will be properly hashed after security class
-
-  if (email === null || username === null || password === null) {
-    notification = "Je moet alle velden invullen.";
-    res.render("register", {
-      title: "Registreren",
-      themaName: themaName,
-      notification: notification,
-    });
-    return;
-  }
-
-  const newUser: Users = {
-    email: email,
-    username: username,
-    password: password,
-    level: 0,
-    about_me: "",
-    profile_picture: "",
-    public_profile: false,
-    collection_more: [
-      {
-        collectionName: "Wenslijst",
-        allGames: [],
-      },
-      {
-        collectionName: "Favorieten",
-        allGames: [],
-      },
-      {
-        collectionName: "Te spelen",
-        allGames: [],
-      },
-      {
-        collectionName: "Momenteel aan het spelen",
-        allGames: [],
-      },
-      {
-        collectionName: "Afgespeeld",
-        allGames: [],
-      },
-    ],
-  };
-
-  try {
-    await client.connect();
-
-    let allUsers: Users[] = await userCollection.find().toArray();
-    let userExists: boolean = allUsers.some(
-      (u) =>
-        u.email.toLowerCase() === email ||
-        u.username.toLowerCase() === username,
-    );
-
-    if (password.length < 12) {
-      notification = "Uw wachtwoord moet minstens 12 karakters bevatten.";
-      res.render("register", {
-        title: "Registreren",
-        themaName: themaName,
-        notification: notification,
-      });
-      return;
-    }
-
-    if (userExists) {
-      notification = "Deze gebruikersnaam of e-mail bestaat al.";
-      res.render("register", {
-        title: "Registreren",
-        themaName: themaName,
-        notification: notification,
-      });
-      return;
-    }
-
-    await userCollection.insertOne(newUser);
-    notification = "Gebruiker succesvol geregistreerd! Je kan nu inloggen.";
-    res.render("register", {
-      title: "Registreren",
-      themaName: themaName,
-      notification: notification,
-    });
-    return;
-  } catch (e) {
-    console.error("Something went wrong with the database.");
-  } finally {
-    await client.close();
-  }
-});
-app.use(profileRouter);
+app.use("/login", loginRoute())
+app.use("/register", registerRoute());
+app.use("/profile", profileRoute());
+app.use("/public-profile", PublicProfileRoute());
 
 app.get("/info", (req, res) => {
   const themaName: string = res.locals.themaName;
@@ -457,14 +290,7 @@ app.get("/compare-games", (req, res) => {
 });
 
 app.listen(app.get("port"), async () => {
-  //mongo client
-  const uriMongo = process.env.MONGODB;
-
-  if (!uriMongo) {
-    throw new Error("MONGODB string not defined in .env!");
-  }
-  client = await MongoClient.connect(uriMongo);
-  userCollection = client.db("users").collection("userinfo");
+  connect();
 
   const response = await fetch(`${url}`);
 
