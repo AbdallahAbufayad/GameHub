75529e73b639c04870dc530a7a50c97bf6292a0a
ElkeK <s167738@ap.be>
2026-04-23 08:30:05 +0000

    Fixed database.ts with more error handling.

--- database.ts
+++ database.ts
@@ -1,38 +1,39 @@
 import { Collection, MongoClient } from "mongodb";
-import { Users, GamesApi, Results, Game } from "./types";
+import { Users } from "./types";
 import { configDotenv } from "dotenv";
 configDotenv();
 
-const uriMongo = process.env.MONGODB;
-
-const url: string = `https://api.rawg.io/api/games?key=30778c23f4f34908a65b042d94443ba7&dates=1969-01-01,2026-04-11`;
-export let counter = 1;
-let gamesOfApi: GamesApi;
-
-if (!uriMongo) {
-  throw new Error("MONGODB string not defined in .env!");
+if (!process.env.MONGODB_URI) {
+  console.error("Mongo string not defined!");
+  process.exit(1);
 }
-let client: MongoClient = new MongoClient(uriMongo);
+
+const client: MongoClient = new MongoClient(process.env.MONGODB_URI);
 let userCollection: Collection<Users> = client
   .db("users")
   .collection("userinfo");
 
-export async function connect() {
+export async function exit() {
   try {
-    await client.connect();
-    console.log("Connected to the database!");
-    process.on("SIGINT", exit);
+    await client.close();
+    console.log("Database closed!");
+    process.exit(0);
   } catch (error) {
     console.error(error);
+  } finally {
+    process.exit(0);
   }
 }
 
-export function exit() {
+export async function connect() {
   try {
-    client.close();
-    console.log("Closed the database!");
+    await client.connect();
+    console.log("Database successfully connected!");
+    process.on("SIGINT", exit);
+    process.on("SIGUSR2", exit);
   } catch (error) {
     console.error(error);
+    process.exit(1);
   }
 }
 
@@ -65,87 +66,4 @@ export async function getUser(emailOrUsername: string) {
   );
 
   return currentUser;
-}
-
-export async function getGames() {
-  gamesOfApi = await fetch(`${url}`).then((res) => res.json());
-  return gamesOfApi;
-}
-
-export function increaseCounter() {
-  counter += 1;
-}
-
-export function decreaseCounter() {
-  counter -= 1;
-}
-
-export async function getRecentGameswithPageSize() {
-  gamesOfApi = await fetch(
-    `${url}&ordering=-released&page=${counter}&page_size=20`,
-  ).then((res) => res.json());
-  return gamesOfApi;
-}
-
-export async function getOrderdGamesByReleaseYearAsc() {
-  gamesOfApi = await fetch(
-    `${url}&ordering=released&page=${counter}&page_size=20`,
-  ).then((res) => res.json());
-  return gamesOfApi;
-}
-
-export async function getOrderdGamesByNameDes() {
-  gamesOfApi = await fetch(
-    `${url}&ordering=-name&page=${counter}&page_size=20`,
-  ).then((res) => res.json());
-  return gamesOfApi;
-}
-
-export async function getOrderdGamesByNameAsc() {
-  gamesOfApi = await fetch(
-    `${url}&ordering=name&page=${counter}&page_size=20`,
-  ).then((res) => res.json());
-  return gamesOfApi;
-}
-
-export async function getOrderdGamesByRatingDes() {
-  gamesOfApi = await fetch(
-    `${url}&ordering=-rating&page=${counter}&page_size=20`,
-  ).then((res) => res.json());
-  return gamesOfApi;
-}
-
-export async function getOrderdGamesByRatingAsc() {
-  gamesOfApi = await fetch(
-    `${url}&ordering=rating&page=${counter}&page_size=20`,
-  ).then((res) => res.json());
-  return gamesOfApi;
-}
-
-export async function getOrderdGamesBySingleplayer() {
-  gamesOfApi = await fetch(
-    `${url}&ordering=singleplayer&page=${counter}&page_size=20`,
-  ).then((res) => res.json());
-  return gamesOfApi;
-}
-
-export async function getOrderdGamesByMultiplayer() {
-  gamesOfApi = await fetch(
-    `${url}&ordering=multiplayer&page=${counter}&page_size=20`,
-  ).then((res) => res.json());
-  return gamesOfApi;
-}
-
-export async function searchGame(searchedGame: string) {
-  gamesOfApi = await fetch(`${url}&search=${searchedGame}`).then((res) =>
-    res.json(),
-  );
-  return gamesOfApi;
-}
-
-export async function getRecentGames() {
-  gamesOfApi = await fetch(`${url}&ordering=-released`).then((res) =>
-    res.json(),
-  );
-  return gamesOfApi;
 }
